42b9f63df1082523b8ee92f1a1ad580dbc0b927b
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-20 12:26:28 +0900

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,74 +2,74 @@ import { useState, useEffect } from 'react'
 import axios from "axios"
 import './App.css'
 import Header from './components/Header'
-import TodoEditor from './components/TodoEditor'
-import TodoList from './components/TodoList'
+import BucketForm from './components/BucketForm'
+import BucketList from './components/BucketList'
+
 function App() {
 
-  const [todos, setTodos] = useState([])
-  const API = `${import.meta.env.VITE_API_URL}/api/todos`
+  const [buckets, setBuckets] = useState([])
+  const API = `${import.meta.env.VITE_API_URL}/api/buckets`
 
   useEffect(() => {
-    const fetchTodos = async () => {
+    const fetchBuckets = async () => {
       try {
         const res = await axios.get(API)
         const data = Array.isArray(res.data) ?
-          res.data : res.data.todos ?? []
+          res.data : res.data.buckets ?? []
 
-        setTodos(data)
+        setBuckets(data)
         console.log(data)
 
       } catch (error) {
         console.log("가져오기 실패", error)
       }
     }
-    fetchTodos()
+    fetchBuckets()
   }, [])
 
 
-  const onCreate = async (todoText) => {
-    if (!todoText.trim()) return
+  const onCreate = async (bucketText) => {
+    if (!bucketText.trim()) return
 
     try {
+      const res = await axios.post(API, { text: bucketText.trim() })
 
-      const res = await axios.post(API, { text: todoText.trim() })
+      const created = res.data?.bucket ?? res.data
 
-      const created = res.data?.todo?? res.data
-
-      if(Array.isArray(res.data?.todos)){
-        setTodos(res.data.todos)
-      }else {
-        setTodos(prev=>[created, ...prev])
+      if (Array.isArray(res.data?.buckets)) {
+        setBuckets(res.data.buckets)
+      } else {
+        setBuckets(prev => [created, ...prev])
       }
 
     } catch (error) {
-      console.log("추가 실패",error)
+      console.log("추가 실패", error)
     }
   }
 
-  const onDelete = async(id)=>{
+  const onDelete = async (id) => {
     try {
-      if(!confirm("정말 삭제할까요?")) return
+      if (!confirm("정말 삭제할까요?")) return
 
-      const {data}=await axios.delete(`${API}/${id}`)
+      const { data } = await axios.delete(`${API}/${id}`)
 
-      if(Array.isArray(data?.todos)){
-        setTodos(data.todos)
+      if (Array.isArray(data?.buckets)) {
+        setBuckets(data.buckets)
         return
       }
 
-      const deletedId = data?.deletedId?? data?.todo?._id ?? data?._id??id
-      setTodos((prev)=> prev.filter((t)=>t._id!==deletedId))
+      const deletedId = data?.deletedId ?? data?.bucket?._id ?? data?._id ?? id
+      setBuckets((prev) => prev.filter((t) => t._id !== deletedId))
     } catch (error) {
-      console.error("삭제 실패",error)
+      console.error("삭제 실패", error)
     }
   }
 
   return (
     <div className='App'>
       <Header />
-      <TodoEditor onCreate={onCreate}/>
-      <TodoList todos={todos} onDelete={onDelete}/>
+      <BucketForm onCreate={onCreate}/>
+      <BucketList buckets={buckets} onDelete={onDelete}/>
     </div>
   )
 }
